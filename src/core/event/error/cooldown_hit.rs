c8bdfc2cdcf1a8516f068ad30c0081baa786ed20
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-02 16:36:14 +0200

    add: error handling module + errors

---
+++ src/core/event/error/cooldown_hit.rs
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2024 Kasutaja
+ *
+ * wakalaka-rs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wakalaka-rs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
+ */
+use crate::util::uses::*;
+
+pub async fn on_cooldown_hit(remaining_cooldown: time::Duration, ctx: crate::Context<'_>) {
+    ctx.reply(&format!(
+        "You're too fast! Try again in {remaining_cooldown:?} seconds."
+    ))
+    .await
+    .unwrap();
+}
