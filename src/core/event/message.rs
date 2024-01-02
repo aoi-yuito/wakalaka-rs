555ae3b0516f48298dd4d15d5f5b04a6b7d36ff9
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-02 22:25:23 +0200

    add: check for attachments, funcs for sending various DMs

--- src/core/event/message.rs
+++ src/core/event/message.rs
@@ -16,8 +16,12 @@
  */
 use crate::util::uses::*;
 
-pub fn is_message_embed(msg: &Message) -> bool {
+pub fn is_embed(msg: &Message) -> bool {
     !msg.embeds.is_empty()
+}
+
+pub fn is_attachment(msg: &Message) -> bool {
+    !msg.attachments.is_empty()
 }
 
 pub async fn on_message(ctx: &Context, msg: &Message) -> Result<(), crate::Error> {
@@ -28,24 +32,102 @@ pub async fn on_message(ctx: &Context, msg: &Message) -> Result<(), crate::Error
     Ok(())
 }
 
-pub async fn send_dm(
+pub async fn send_dm_raw(
     http: Arc<Http>,
     user_id: UserId,
     s: &str,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+) -> Result<(), Box<dyn error::Error + Send + Sync>> {
     let dm_channel = user_id.create_dm_channel(&http).await?;
     dm_channel.say(&http, s).await?;
 
     Ok(())
 }
 
-pub async fn send_dm_as_embed(
+pub async fn send_dm(
     http: Arc<Http>,
     user_id: UserId,
     message: CreateMessage,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+) -> Result<(), Box<dyn error::Error + Send + Sync>> {
     let dm_channel = user_id.create_dm_channel(&http).await?;
     dm_channel.send_message(&http, message).await?;
+
+    Ok(())
+}
+
+pub async fn send_dm_if_embed_attachment_raw(
+    reaction: Reaction,
+    channel_id: u64,
+    http: Arc<Http>,
+    ctx: &Context,
+    mag_right: ReactionType,
+    s: &str,
+) -> Result<(), Box<dyn error::Error + Send + Sync>> {
+    let reaction_channel_id = reaction.channel_id;
+    if reaction_channel_id != ChannelId::from(channel_id) {
+        return Ok(());
+    }
+
+    let message_id = reaction.message_id;
+    let reactive_message = http
+        .get_message(reaction_channel_id, message_id)
+        .await
+        .expect("Failed to get message");
+
+    let user_id = reaction.user_id;
+
+    if event::message::is_attachment(&reactive_message)
+        || event::message::is_embed(&reactive_message)
+    {
+        let users = reactive_message
+            .reaction_users(&ctx, mag_right.clone(), None, None)
+            .await?;
+        for user in users {
+            if user.bot {
+                continue;
+            }
+
+            let _ = event::message::send_dm_raw(http.clone(), user_id.unwrap(), s).await;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn send_dm_if_embed_attachment(
+    reaction: Reaction,
+    channel_id: u64,
+    http: Arc<Http>,
+    ctx: &Context,
+    mag_right: ReactionType,
+    message: CreateMessage,
+) -> Result<(), Box<dyn error::Error + Send + Sync>> {
+    let reaction_channel_id = reaction.channel_id;
+    if reaction_channel_id != ChannelId::from(channel_id) {
+        return Ok(());
+    }
+
+    let message_id = reaction.message_id;
+    let reactive_message = http
+        .get_message(reaction_channel_id, message_id)
+        .await
+        .expect("Failed to get message");
+
+    let user_id = reaction.user_id;
+
+    if event::message::is_attachment(&reactive_message)
+        || event::message::is_embed(&reactive_message)
+    {
+        let users = reactive_message
+            .reaction_users(&ctx, mag_right.clone(), None, None)
+            .await?;
+        for user in users {
+            if user.bot {
+                continue;
+            }
+
+            let _ = event::message::send_dm(http.clone(), user_id.unwrap(), message.clone()).await;
+        }
+    }
 
     Ok(())
 }
