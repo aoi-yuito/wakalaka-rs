9a08445065019ab70a1bf3372eaf90a30c41f63d
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-02 20:02:17 +0200

    add&remove: imports related to `Embed` struct & `LinkedHashMap` crate

--- src/util/uses.rs
+++ src/util/uses.rs
@@ -19,25 +19,24 @@ pub use crate::booru::*;
 pub use crate::core::embed;
 pub use crate::core::event;
 pub use crate::core::framework;
-pub use crate::embed::Embed;
 pub use crate::files::metadata::FileMetadata;
 pub use crate::util::files;
 pub use crate::util::settings::Settings;
 pub use crate::util::strings;
 pub use chrono::{Duration, NaiveDate};
+pub use linked_hash_map::LinkedHashMap;
 pub use png::{Decoder, Info, Transformations};
 pub use poise::async_trait;
 pub use poise::builtins;
 pub use poise::serenity_prelude::{
     self as serenity, Activity, Attachment, CacheHttp, ChannelId, ClientBuilder, Context,
     CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter, CreateMessage, EventHandler, FullEvent,
-    GatewayIntents, GuildId, Http, Message, MessageId, Permissions, ReactionType, Ready,
+    GatewayIntents, GuildId, Http, Message, MessageBuilder, MessageId, Permissions, Reaction,
+    ReactionType, Ready, User, UserId,
 };
 pub use poise::{ApplicationContext, CreateReply, Framework, FrameworkError, FrameworkOptions};
 pub use serde::{Deserialize, Serialize};
 pub use serde_json::Map;
-pub use std::collections::hash_map::IntoIter;
-pub use std::collections::HashMap;
 pub use std::env;
 pub use std::fs;
 pub use std::fs::{File, Metadata, OpenOptions};
