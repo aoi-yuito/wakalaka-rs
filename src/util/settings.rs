b612e0459514b7579219e750ef9d898c2baa3b5a
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-02 21:18:36 +0200

    edit: rework the settings; add: `metadata_channel_id`

--- src/util/settings.rs
+++ src/util/settings.rs
@@ -16,78 +16,98 @@
  */
 use crate::util::uses::*;
 
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Settings {
-    #[serde(alias = "APPLICATION_ID")]
-    pub application_id: i64,
+const SETTINGS_TOML: &str = "Settings.toml";
 
-    #[serde(alias = "DISCORD_TOKEN")]
-    pub discord_token: String,
+#[derive(Deserialize, Serialize)]
+pub struct Settings {
+    pub application_id: i64,
+    pub client_id: i64,
+    pub token: String,
+    pub metadata_channel_id: i64,
 }
 
 impl Settings {
     pub fn new() -> Result<Self, Box<dyn error::Error>> {
-        let mut settings = Self::read()?;
-        if settings.discord_token.is_empty() {
-            if let Ok(token) = env::var("DISCORD_TOKEN") {
-                settings.discord_token = token;
-                Self::write(&settings)?;
+        let mut settings = Self::read_settings()?;
+        if settings.token.is_empty() {
+            if let Ok(discord_token) = env::var("DISCORD_TOKEN") {
+                settings.token = discord_token;
+
+                Self::write_settings(&settings)?;
             } else {
                 println!("DISCORD_TOKEN not found in environment variables");
                 println!("Please enter the Token found in the Developer Portal:");
 
-                let mut token = String::new();
-                io::stdin().read_line(&mut token)?;
-                token = token.trim().to_owned();
+                let mut input_token = String::new();
+                io::stdin().read_line(&mut input_token)?;
+                input_token = input_token.trim().to_owned();
 
-                settings.discord_token = token;
-                Self::write(&settings)?;
+                settings.token = input_token;
+                Self::write_settings(&settings)?;
             }
         }
         Ok(settings)
     }
 
-    fn write(settings: &Self) -> Result<(), Box<dyn error::Error>> {
-        if !files::exists("Settings.toml") {
-            File::create("Settings.toml")?;
-        }
+    pub fn read_settings() -> Result<Self, Box<dyn error::Error>> {
+        let application_id = Self::read_section("Developer", "application_id")?
+            .parse::<i64>()
+            .map_err(|_| "application_id is not an integer")?;
+        let client_id = Self::read_section("Developer", "client_id")?
+            .parse::<i64>()
+            .map_err(|_| "client_id is not an integer")?;
+        let token = Self::read_section("Developer", "token")?;
 
-        let contents = toml::to_string(settings)?;
-        fs::write("Settings.toml", contents)?;
-        Ok(())
-    }
-
-    fn read() -> Result<Self, Box<dyn error::Error>> {
-        if !files::exists("Settings.toml") {
-            let default_settings = Self {
-                application_id: 0,
-                discord_token: String::new(),
-            };
-            Self::write(&default_settings)?;
-        }
-
-        let contents = fs::read_to_string("Settings.toml")?;
-        let value: toml::Value = toml::from_str(&contents)?;
-
-        let authorisation = value
-            .get("Authorisation")
-            .ok_or("'Authorisation' section not found")?;
-        let discord_token = authorisation
-            .get("discord_token")
-            .ok_or("'discord_token' not found in 'Authorisation' section")?
-            .as_str()
-            .ok_or("'discord_token' is not a string")?
-            .to_owned();
-        let application_id = authorisation
-            .get("application_id")
-            .ok_or("'application_id' not found in 'Authorisation' section")?
-            .as_integer()
-            .ok_or("'application_id' is not an integer")?;
+        let metadata_channel_id = Self::read_section("Channels", "metadata_channel_id")?
+            .parse::<i64>()
+            .map_err(|_| "metadata_channel_id is not an integer")?;
 
         let settings = Self {
             application_id,
-            discord_token,
+            client_id,
+            token,
+            metadata_channel_id,
         };
         Ok(settings)
+    }
+
+    pub fn write_settings(&self) -> Result<(), Box<dyn error::Error>> {
+        if !files::exists(SETTINGS_TOML) {
+            File::create(SETTINGS_TOML)?;
+        }
+
+        let contents = toml::to_string(&self)?;
+        fs::write(SETTINGS_TOML, contents)?;
+
+        Ok(())
+    }
+
+    fn read_section(section: &str, key: &'static str) -> Result<String, Box<dyn error::Error>> {
+        if !files::exists(SETTINGS_TOML) {
+            let default_settings = Self {
+                application_id: 0,
+                client_id: 0,
+                token: String::new(),
+                metadata_channel_id: 0,
+            };
+
+            Self::write_settings(&default_settings)?;
+        }
+
+        let contents = fs::read_to_string(SETTINGS_TOML)?;
+        let value: toml::Value = toml::from_str(&contents)?;
+
+        let section = value
+            .get(section)
+            .ok_or(format!("'{}' section not found", section))?;
+        let key = section
+            .get(key)
+            .ok_or(format!("'{}' not found in '{}' section", key, section))?;
+
+        let value = match key {
+            toml::Value::String(s) => s.clone(),
+            _ => key.to_string(),
+        };
+        Ok(value)
     }
 }
