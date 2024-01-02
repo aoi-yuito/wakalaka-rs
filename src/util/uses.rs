3c5405b0196bac7b45d05cc45f4add217fc202fd
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-02 08:59:49 +0200

    edit: imports for `framework.rs`

--- src/util/uses.rs
+++ src/util/uses.rs
@@ -2,7 +2,8 @@ pub use crate::booru::aibooru;
 pub use crate::booru::*;
 pub use crate::core::embed;
 pub use crate::core::event;
-pub use crate::files::FileMetadata;
+pub use crate::core::framework;
+pub use crate::files::metadata::FileMetadata;
 pub use crate::util::files;
 pub use crate::util::settings::Settings;
 pub use crate::util::strings;
@@ -11,12 +12,13 @@ pub use png::{Decoder, Info, Transformations};
 pub use poise::async_trait;
 pub use poise::serenity_prelude::{
     self as serenity, Activity, Attachment, ClientBuilder, Context, CreateEmbed, CreateEmbedAuthor,
-    CreateEmbedFooter, CreateMessage, EventHandler, Framework, FullEvent, GatewayIntents, GuildId,
-    Message, Ready,
+    CreateEmbedFooter, CreateMessage, EventHandler, FullEvent, GatewayIntents, GuildId, Message,
+    Ready,
 };
-pub use poise::CreateReply;
+pub use poise::{CreateReply, Framework, FrameworkOptions};
 pub use serde::{Deserialize, Serialize};
 pub use serde_json::Map;
+pub use std::collections::hash_map::IntoIter;
 pub use std::collections::HashMap;
 pub use std::env;
 pub use std::fs;
