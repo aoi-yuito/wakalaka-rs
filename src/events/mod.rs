cfc98e64ff550cbc797120d9d84920a1ca851a25
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-05 04:08:22 +0200

    edit: separate event(s) into file(s)

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -13,13 +13,15 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
+pub mod cache_ready;
+pub mod interaction_create;
+pub mod ready;
+
 use serenity::{
-    all::{CommandInteraction, GuildId, Interaction, Ready},
+    all::{GuildId, Interaction, Ready},
     async_trait,
-    builder::{CreateCommand, CreateInteractionResponse, CreateInteractionResponseMessage},
     client::EventHandler,
 };
-use tracing::{log::error, log::info};
 
 use crate::commands::*;
 use crate::Context;
@@ -29,100 +31,14 @@ pub struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::Command(command) = interaction {
-            let command_user = &command.user.name;
-            let command_name = &command.data.name;
-            let channel_name = &command.channel_id.name(&ctx).await.unwrap_or_else(|why| {
-                error!("{why}");
-                panic!("Error while retrieving channel name");
-            });
-            info!("@{command_user} executed {command_name:?} in #{channel_name}");
-
-            let content = register_command(&ctx, &command).await;
-            register_slash_commands(&ctx, &command, content).await;
-        }
+        interaction_create::handle(ctx, interaction).await;
     }
 
     async fn cache_ready(&self, ctx: Context, guilds: Vec<GuildId>) {
-        let guild_count = guilds.len();
-        info!("Prepared cache for {guild_count} guild(s)");
-
-        for guild in guilds {
-            let guild_name = guild.name(&ctx).unwrap_or_else(|| {
-                error!("No guild name found");
-                panic!("Error while retrieving guild name");
-            });
-            info!("\t{guild_name}");
-        }
-
-        let cache = &ctx.cache;
-
-        let guild_ids = cache.guilds();
-        for guild_id in guild_ids {
-            let (guild_name, guild_member_count, guild_role_count, guild_channel_count) = {
-                let guild = cache.guild(guild_id).unwrap_or_else(|| {
-                    error!("No guild found");
-                    panic!("Error while retrieving guild");
-                });
-
-                (
-                    guild.name.clone(),
-                    guild.members.len(),
-                    guild.roles.len(),
-                    guild.channels.len(),
-                )
-            };
-            info!("Connected to {guild_name}");
-            info!("\t{guild_name} has {guild_member_count} members");
-            info!("\t{guild_name} has {guild_role_count} roles");
-            info!("\t{guild_name} has {guild_channel_count} channels");
-
-            let registered_commands = guild_id.set_commands(&ctx.http, created_commands()).await;
-            if let Ok(registered_commands) = registered_commands {
-                let registered_command_count = &registered_commands.len();
-                info!("Registered {registered_command_count} command(s) in {guild_name}");
-
-                for registered_command in registered_commands {
-                    let registered_command_name = &registered_command.name;
-                    let registered_command_description = &registered_command.description;
-                    info!("\t{registered_command_name:?} - {registered_command_description}");
-                }
-            }
-        }
+        cache_ready::handle(ctx, guilds).await;
     }
 
     async fn ready(&self, ctx: Context, ready: Ready) {
-        let user_name = &ready.user.name;
-        info!("Logged in as @{user_name}");
-
-        let cache = &ctx.cache;
-    }
-}
-
-fn created_commands() -> Vec<CreateCommand> {
-    vec![core::restart::register()]
-}
-
-async fn register_slash_commands(
-    ctx: &Context,
-    command: &CommandInteraction,
-    content: Option<String>,
-) {
-    if let Some(content) = content {
-        let data = CreateInteractionResponseMessage::new().content(content);
-        let builder = CreateInteractionResponse::Message(data);
-        if let Err(why) = command.create_response(&ctx.http, builder).await {
-            error!("{why}")
-        }
-    }
-}
-
-async fn register_command(ctx: &Context, command: &CommandInteraction) -> Option<String> {
-    let command_data_name = &command.data.name;
-    let command_data_options = &command.data.options();
-
-    match command_data_name.as_str() {
-        "restart" => Some(core::restart::run(&ctx, command, command_data_options).await),
-        _ => Some("Unknown command.".to_string()),
+        ready::handle(ctx, ready).await;
     }
 }
