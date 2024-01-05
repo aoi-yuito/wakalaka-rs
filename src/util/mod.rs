a386b0eb7de86ddbd09ec7a43649c38948836979
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-05 22:38:41 +0200

    edit: ...

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -12,6 +12,5 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
-
 pub mod config;
 pub mod files;
