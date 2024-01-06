a1d0c94e8b8d388b2b5bfc3f6206ad63e14f8609
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-06 16:36:35 +0200

    add: user agent, colour, "wiki" cmd

--- src/commands/web/booru/danbooru.rs
+++ src/commands/web/booru/danbooru.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
-use super::BooruPost;
+use super::{BooruPost, BooruWikiPages};
 use crate::{commands, Context};
 use serenity::{
     all::{CommandInteraction, CommandOptionType, ResolvedOption, ResolvedValue},
@@ -27,6 +27,10 @@ const DANBOORU_URL: &str = "https://danbooru.donmai.us";
 const DANBOORU_LOGO_PNG: &str =
     "https://danbooru.donmai.us/packs/static/images/danbooru-logo-128x128-ea111b6658173e847734.png";
 
+const DANBOORU_COLOUR: u32 = 0xAC8A64;
+
+const POSTMAN_USER_AGENT: &str = "PostmanRuntime/7.36.0";
+
 pub async fn run(
     ctx: &Context,
     interaction: &CommandInteraction,
@@ -35,17 +39,60 @@ pub async fn run(
     let command = commands::command(interaction, 0);
     match command.name.as_str() {
         "post" => post(ctx, options, interaction).await,
+        "wiki" => wiki(ctx, options, interaction).await,
         _ => None,
     }
 }
 
-async fn id(options: &[ResolvedOption<'_>]) -> Option<String> {
-    for option in options {
-        if let ResolvedValue::SubCommand(subcommand) = &option.value {
-            let id = subcommand.get(0)?;
-            if let ResolvedValue::Integer(id) = &id.value {
-                return Some(id.to_string());
-            }
+async fn wiki(
+    ctx: &Context,
+    options: &[ResolvedOption<'_>],
+    interaction: &CommandInteraction,
+) -> Option<String> {
+    let tag = tag(options)
+        .await
+        .unwrap_or_default()
+        .replace(" ", "_")
+        .to_lowercase();
+    let channel_id = interaction.channel_id;
+    let tag_exists = BooruWikiPages::tag_exists(ctx, channel_id, &tag).await;
+    if tag_exists {
+        let wiki_pages_show = format!("{DANBOORU_URL}/wiki_pages/{tag}");
+        let wiki_pages_show_json = format!("{wiki_pages_show}.json");
+
+        let client = reqwest::Client::new();
+
+        let response_text = client
+            .get(&wiki_pages_show_json)
+            .header(reqwest::header::USER_AGENT, POSTMAN_USER_AGENT) // Fucking Cloudflare...
+            .send()
+            .await
+            .expect("Error while sending GET request")
+            .text()
+            .await
+            .expect("Error while getting text from response");
+        let response_json =
+            serde_json::from_str(&response_text).expect("Error while parsing JSON from response");
+
+        let success = super::has_success(ctx, &response_json, channel_id).await;
+        if success {
+            let wiki_pages = BooruWikiPages::new(DANBOORU_URL, &response_json);
+            let wiki_pages_title = &wiki_pages.title;
+
+            let embed = BooruWikiPages::embed(
+                &wiki_pages,
+                &wiki_pages_title,
+                DANBOORU_URL,
+                BooruWikiPages::embed_footer(&wiki_pages),
+                DANBOORU_COLOUR,
+            );
+
+            let response_message = CreateInteractionResponseMessage::default();
+
+            let message = response_message.add_embed(embed);
+            let response = CreateInteractionResponse::Message(message);
+
+            let _ = interaction.create_response(&ctx.http, response).await;
         }
     }
     None
@@ -63,7 +110,7 @@ async fn post(
         .expect("Error while parsing ID");
     let channel_id = interaction.channel_id;
 
-    let exists = BooruPost::exists(ctx, channel_id, id).await;
+    let exists = BooruPost::post_exists(ctx, channel_id, id).await;
     if exists {
         let posts_json = format!("{DANBOORU_URL}/posts/{id}.json");
 
@@ -71,7 +118,7 @@ async fn post(
 
         let response_text = client
             .get(&posts_json)
-            .header(reqwest::header::USER_AGENT, "PostmanRuntime/7.36.0") // Fucking Cloudflare...
+            .header(reqwest::header::USER_AGENT, POSTMAN_USER_AGENT) // Fucking Cloudflare...
             .send()
             .await
             .expect("Error while sending GET request")
@@ -81,9 +128,10 @@ async fn post(
         let response_json =
             serde_json::from_str(&response_text).expect("Error while parsing JSON from response");
 
-        let success = BooruPost::has_success(ctx, &response_json, channel_id, id).await;
+        let success = super::has_success(ctx, &response_json, channel_id).await;
         if success {
             let post = BooruPost::new(&response_json);
+            let post_file_url = &post.file_url;
 
             let embed = BooruPost::embed(
                 &post,
@@ -91,9 +139,9 @@ async fn post(
                 id,
                 None,
                 DANBOORU_URL,
-                post.file_url.clone(),
-                BooruPost::generate_footer(&post),
-                0xAC8A64,
+                post_file_url,
+                BooruPost::embed_footer(&post),
+                DANBOORU_COLOUR,
             );
 
             let response_message = CreateInteractionResponseMessage::default();
@@ -107,6 +155,30 @@ async fn post(
     None
 }
 
+async fn tag(options: &[ResolvedOption<'_>]) -> Option<String> {
+    for option in options {
+        if let ResolvedValue::SubCommand(subcommand) = &option.value {
+            let tags = subcommand.get(0)?;
+            if let ResolvedValue::String(tags) = &tags.value {
+                return Some(tags.to_string());
+            }
+        }
+    }
+    None
+}
+
+async fn id(options: &[ResolvedOption<'_>]) -> Option<String> {
+    for option in options {
+        if let ResolvedValue::SubCommand(subcommand) = &option.value {
+            let id = subcommand.get(0)?;
+            if let ResolvedValue::Integer(id) = &id.value {
+                return Some(id.to_string());
+            }
+        }
+    }
+    None
+}
+
 pub fn register() -> CreateCommand {
     CreateCommand::new("danbooru")
         .description(format!("Provides interactibility with {DANBOORU_URL:?}").as_str())
@@ -114,10 +186,21 @@ pub fn register() -> CreateCommand {
             CreateCommandOption::new(
                 CommandOptionType::SubCommand,
                 "post",
-                "Previews a post from Danbooru",
+                "Previews a post from Danbooru.",
             )
             .add_sub_option(
                 CreateCommandOption::new(CommandOptionType::Integer, "id", "Index of post.")
+                    .required(true),
+            ),
+        )
+        .add_option(
+            CreateCommandOption::new(
+                CommandOptionType::SubCommand,
+                "wiki",
+                "Previews a wiki page from Danbooru.",
+            )
+            .add_sub_option(
+                CreateCommandOption::new(CommandOptionType::String, "tag", "Tag of wiki page.")
                     .required(true),
             ),
         )
