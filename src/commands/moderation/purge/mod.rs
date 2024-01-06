e1bb526f75772f50571062aa981cf8caa4512835
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-06 19:54:56 +0200

    add: "purge" command

--- src/commands/moderation/purge/mod.rs
+++ src/commands/moderation/purge/mod.rs
@@ -12,3 +12,39 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
+
+mod count;
+
+use serenity::all::CommandInteraction;
+use serenity::all::CommandOptionType;
+use serenity::builder::CreateCommand;
+use serenity::builder::CreateCommandOption;
+use serenity::all::ResolvedOption;
+
+use crate::commands;
+use crate::Context;
+
+pub async fn run(
+    ctx: &Context,
+    interaction: &CommandInteraction,
+    options: &[ResolvedOption<'_>]
+) -> Option<String> {
+    let command = commands::command(interaction, 0);
+    match command.name.as_str() {
+        "count" => count::count(ctx, interaction, options).await,
+        _ => None,
+    }
+}
+
+// /purge <o:count> <o:user> <o:channel>
+pub fn register() -> CreateCommand {
+    CreateCommand::new("purge")
+        .description("Purges messages from channel.")
+        .add_option(
+            CreateCommandOption::new(
+                CommandOptionType::Integer,
+                "count",
+                "Number of messages to purge."
+            ).required(true)
+        )
+}
