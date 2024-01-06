5f355128f6306cb3c55e89cae223f7812fbb1b7c
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-06 16:36:41 +0200

    add: colour, "wiki" cmd

--- src/commands/web/booru/aibooru.rs
+++ src/commands/web/booru/aibooru.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
-use super::BooruPost;
+use super::{BooruPost, BooruWikiPages};
 use crate::{commands, Context};
 use serenity::{
     all::{CommandInteraction, CommandOptionType, ResolvedOption, ResolvedValue},
@@ -27,6 +27,8 @@ const AIBOORU_URL: &str = "https://aibooru.online";
 const AIBOORU_PNG_LOGO_URL: &str =
     "https://aibooru.online/packs/static/images/danbooru-logo-128x128-5dfe4b292bd64a786b41.png";
 
+const AIBOORU_COLOUR: u32 = 0x77B91E;
+
 pub async fn run(
     ctx: &Context,
     interaction: &CommandInteraction,
@@ -35,17 +37,59 @@ pub async fn run(
     let command = commands::command(interaction, 0);
     match command.name.as_str() {
         "post" => post(ctx, options, interaction).await,
+        "wiki" => wiki(ctx, options, interaction).await,
         _ => None,
     }
 }
 
-async fn id(options: &[ResolvedOption<'_>]) -> Option<String> {
-    for option in options {
-        if let ResolvedValue::SubCommand(subcommand) = &option.value {
-            let id = subcommand.get(0)?;
-            if let ResolvedValue::Integer(id) = &id.value {
-                return Some(id.to_string());
-            }
+async fn wiki(
+    ctx: &Context,
+    options: &[ResolvedOption<'_>],
+    interaction: &CommandInteraction,
+) -> Option<String> {
+    let tag = tag(options)
+        .await
+        .unwrap_or_default()
+        .replace(" ", "_")
+        .to_lowercase();
+    let channel_id = interaction.channel_id;
+    let tag_exists = BooruWikiPages::tag_exists(ctx, channel_id, &tag).await;
+    if tag_exists {
+        let wiki_pages_show = format!("{AIBOORU_URL}/wiki_pages/{tag}");
+        let wiki_pages_show_json = format!("{wiki_pages_show}.json");
+
+        let client = reqwest::Client::new();
+
+        let response_text = client
+            .get(&wiki_pages_show_json)
+            .send()
+            .await
+            .expect("Error while sending GET request")
+            .text()
+            .await
+            .expect("Error while getting text from response");
+        let response_json =
+            serde_json::from_str(&response_text).expect("Error while parsing JSON from response");
+
+        let success = super::has_success(ctx, &response_json, channel_id).await;
+        if success {
+            let wiki_pages = BooruWikiPages::new(AIBOORU_URL, &response_json);
+            let wiki_pages_title = &wiki_pages.title;
+
+            let embed = BooruWikiPages::embed(
+                &wiki_pages,
+                &wiki_pages_title,
+                AIBOORU_URL,
+                BooruWikiPages::embed_footer(&wiki_pages),
+                AIBOORU_COLOUR,
+            );
+
+            let response_message = CreateInteractionResponseMessage::default();
+
+            let message = response_message.add_embed(embed);
+            let response = CreateInteractionResponse::Message(message);
+
+            let _ = interaction.create_response(&ctx.http, response).await;
         }
     }
     None
@@ -63,8 +107,8 @@ async fn post(
         .expect("Error while parsing ID");
     let channel_id = interaction.channel_id;
 
-    let exists = BooruPost::exists(ctx, channel_id, id).await;
-    if exists {
+    let post_exists = BooruPost::post_exists(ctx, channel_id, id).await;
+    if post_exists {
         let posts_show_json = format!("{AIBOORU_URL}/posts/{id}.json");
 
         let client = reqwest::Client::new();
@@ -80,9 +124,10 @@ async fn post(
         let response_json =
             serde_json::from_str(&response_text).expect("Error while parsing JSON from response");
 
-        let success = BooruPost::has_success(ctx, &response_json, channel_id, id).await;
+        let success = super::has_success(ctx, &response_json, channel_id).await;
         if success {
             let post = BooruPost::new(&response_json);
+            let post_file_url = &post.file_url;
 
             let embed = BooruPost::embed(
                 &post,
@@ -90,9 +135,9 @@ async fn post(
                 id,
                 None,
                 AIBOORU_URL,
-                post.file_url.clone(),
-                BooruPost::generate_footer(&post),
-                0x77B91E,
+                post_file_url,
+                BooruPost::embed_footer(&post),
+                AIBOORU_COLOUR,
             );
 
             let response_message = CreateInteractionResponseMessage::default();
@@ -106,6 +151,32 @@ async fn post(
     None
 }
 
+async fn tag(options: &[ResolvedOption<'_>]) -> Option<String> {
+    for option in options {
+        if let ResolvedValue::SubCommand(subcommand) = &option.value {
+            let tags = subcommand.get(0)?;
+            if let ResolvedValue::String(tags) = &tags.value {
+                return Some(tags.to_string());
+            }
+        }
+    }
+
+    None
+}
+
+async fn id(options: &[ResolvedOption<'_>]) -> Option<String> {
+    for option in options {
+        if let ResolvedValue::SubCommand(subcommand) = &option.value {
+            let id = subcommand.get(0)?;
+            if let ResolvedValue::Integer(id) = &id.value {
+                return Some(id.to_string());
+            }
+        }
+    }
+
+    None
+}
+
 pub fn register() -> CreateCommand {
     CreateCommand::new("aibooru")
         .description(format!("Provides interactibility with {AIBOORU_URL:?}").as_str())
@@ -113,10 +184,21 @@ pub fn register() -> CreateCommand {
             CreateCommandOption::new(
                 CommandOptionType::SubCommand,
                 "post",
-                "Previews a post from AIBooru",
+                "Previews a post from AIBooru.",
             )
             .add_sub_option(
                 CreateCommandOption::new(CommandOptionType::Integer, "id", "Index of post.")
+                    .required(true),
+            ),
+        )
+        .add_option(
+            CreateCommandOption::new(
+                CommandOptionType::SubCommand,
+                "wiki",
+                "Previews a tag from AIBooru wiki pages.",
+            )
+            .add_sub_option(
+                CreateCommandOption::new(CommandOptionType::String, "tag", "Name of tag.")
                     .required(true),
             ),
         )
