61771764f90f7dc180c86e8803ac9f3000f6aff2
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-09 17:17:20 +0200

    add: `purge` cmd

---
+++ src/commands/moderator/purge.rs
@@ -0,0 +1,63 @@
+// Copyright (C) 2024 Kawaxte
+// 
+// wakalaka-rs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// wakalaka-rs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
+
+use poise::CreateReply;
+use serenity::builder::GetMessages;
+use tracing::info;
+
+use crate::{Context, Error, check_manage_messages_permission};
+
+///Deletes messages from specified channel.
+#[poise::command(slash_command)]
+pub(crate) async fn purge(ctx: Context<'_>,
+    #[description = "Amount of messages to delete."]
+    count: u8,
+) -> Result<(), Error> {
+    check_manage_messages_permission!(ctx);
+
+    if count < 1 || count > 100 {
+        let message = format!("Sorry, but you can only delete between  and 100 messages at a time.");
+        let _ = ctx.reply(message).await;
+
+        return Ok(());
+    }
+
+    let user_name = &ctx.author().name;
+
+    let channel_id = ctx.channel_id();
+    let channel_name = channel_id.name(&ctx.http()).await?;
+
+    let messages = GetMessages::default().limit(count);
+    let channel_messages = channel_id.messages(&ctx.http(), messages).await?;
+
+    let mut deleted_messages_count = 0;
+
+    for channel_message in channel_messages {
+        channel_message.delete(&ctx.http()).await?;
+
+        deleted_messages_count += 1;
+    }
+
+    let reply = CreateReply {
+        content: Some(format!("I've deleted {deleted_messages_count} message(s) from <#{channel_id}>")),
+        ephemeral: Some(true),
+        ..Default::default()
+    };
+    let _ = ctx.send(reply).await;
+
+    info!("@{user_name} deleted {deleted_messages_count} message(s) from #{channel_name}");
+
+    Ok(())
+}
