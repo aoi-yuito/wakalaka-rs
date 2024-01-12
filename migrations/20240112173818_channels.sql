fe8fda7c6681e6eaab006ac27bb5e6ae00dfdc40
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-13 01:22:35 +0200

    fix: `INTEGER` on `type` should be `VARCHAR`

--- migrations/20240112173818_channels.sql
+++ migrations/20240112173818_channels.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS channels (
     id BIGINT NOT NULL,
-    type INTEGER NOT NULL,
+    type VARCHAR NOT NULL,
     guild_id BIGINT NOT NULL,
     rate_limit_per_user INTEGER NOT NULL,
     FOREIGN KEY (guild_id) REFERENCES guilds(id),
