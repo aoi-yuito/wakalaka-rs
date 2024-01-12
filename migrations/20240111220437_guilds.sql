2fc0efbbbd89535e7693ecc618fcbc18af46c498
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-13 00:19:23 +0200

    remove: old migrations

--- migrations/20240111220437_guilds.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Guilds (
-    id BIGINT NOT NULL,
-    ownerId BIGINT NOT NULL,
-    preferredLocale TEXT,
-    isUnavailable BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (ownerId) REFERENCES Users (id),
-    PRIMARY KEY (id)
-);
--- migrations/20240111220522_guild_channels.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Channels (
-    id BIGINT NOT NULL,
-    parentId BIGINT NOT NULL,
-    guildId BIGINT NOT NULL,
-    position INTEGER NOT NULL,
-    FOREIGN KEY (guildId) REFERENCES Guilds(id),
-    FOREIGN KEY (parentId) REFERENCES Channels(id),
-    PRIMARY KEY (id)
-);
