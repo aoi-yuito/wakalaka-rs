b10e428230258eb5190c275ebc49448aef9ca8ad
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-13 04:39:34 +0200

    edit: assumingly delete everything

--- migrations/20240112171513_guilds.sql
+++ migrations/20240112171513_guilds.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS guilds (
     id BIGINT NOT NULL,
     owner_id BIGINT NOT NULL,
     preferred_locale VARCHAR NOT NULL DEFAULT 'en-US',
-    FOREIGN KEY (owner_id) REFERENCES users(id),
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 )
