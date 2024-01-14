02e85318227085e630a7dcfed9c63c167d7d486e
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-14 03:04:27 +0200

    Edit: ...

--- src/framework/options/event_handler/message.rs
+++ src/framework/options/event_handler/message.rs
@@ -1,24 +1,22 @@
 // Copyright (C) 2024 Kawaxte
-// 
+//
 // wakalaka-rs is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // wakalaka-rs is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
-use serenity::all::Message;
 use poise::serenity_prelude::Context;
+use serenity::all::Message;
 
-pub(crate) async fn handle(msg: &Message, _: &Context) {
-    let author_bot = msg.author.bot;
-    if author_bot {
-        return;
-    }
+use crate::Data;
+
+pub(crate) async fn handle(msg: &Message, ctx: &Context, data: &Data) {
 }
