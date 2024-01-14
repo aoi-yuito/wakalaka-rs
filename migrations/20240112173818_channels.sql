bf848bd2383da144abf244e647dc35be5a4a60e6
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-14 03:02:28 +0200

    remove: old database

--- migrations/20240112173818_channels.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS channels (
-    id BIGINT NOT NULL,
-    type VARCHAR NOT NULL,
-    guild_id BIGINT NOT NULL,
-    rate_limit_per_user INTEGER NOT NULL,
-    FOREIGN KEY (guild_id) REFERENCES guilds(id),
-    PRIMARY KEY (id)
-)
