6cd626448c67efda36aba0051d3807787083e40a
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-16 17:58:10 +0200

    add `?` and `::` as prefixes

--- src/framework/mod.rs
+++ src/framework/mod.rs
@@ -1,25 +1,25 @@
 // Copyright (C) 2024 Kawaxte
-// 
+//
 // wakalaka-rs is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // wakalaka-rs is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
-use poise::{Framework, FrameworkOptions};
+use poise::{Framework, FrameworkOptions, Prefix, PrefixFrameworkOptions};
 use serenity::all::GatewayIntents;
 use tokio::time::Instant;
 use tracing::info;
 
-use crate::{Data, Error};
 use crate::framework::options::commands;
+use crate::{Data, Error};
 
 pub(super) mod options;
 pub(super) mod setup;
@@ -28,22 +28,21 @@ pub(crate) async fn initialise_framework(data: Data) -> Framework<Data, Error> {
     let start_time = Instant::now();
 
     let framework = Framework::builder()
-    .setup(|ctx, _, _| {
-        Box::pin(async move {
-            setup::handle(ctx, data).await
+        .setup(|ctx, _, _| Box::pin(async move { setup::handle(ctx, data).await }))
+        .options(FrameworkOptions {
+            commands: commands::guild_commands().await,
+            prefix_options: PrefixFrameworkOptions {
+                prefix: Some(format!("?")),
+                additional_prefixes: vec![Prefix::Literal("::")], // Inspired by RuneScape's command prefix.
+                ..Default::default()
+            },
+            post_command: |ctx| Box::pin(options::post_command::handle(ctx)),
+            event_handler: |ctx, event, framework, data| {
+                Box::pin(options::event_handler::handle(ctx, event, framework, data))
+            },
+            ..Default::default()
         })
-    })
-    .options(FrameworkOptions {
-        commands: commands::guild_commands().await,
-        post_command: |ctx| Box::pin(options::post_command::handle(ctx)),
-        event_handler: |ctx, event, framework, data| {
-            Box::pin(options::event_handler::handle(
-                ctx, event, framework, data,
-            ))
-        },
-        ..Default::default()
-    })
-    .build();
+        .build();
 
     let elapsed_time = start_time.elapsed();
     info!("Initialised framework in {elapsed_time:.2?}");
