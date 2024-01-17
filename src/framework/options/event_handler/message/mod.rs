d21715f2577f68a8d01a350fc051c9c4169cb349
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-17 20:44:51 +0200

    relocate events to respective folders

--- src/framework/options/event_handler/message/mod.rs
+++ src/framework/options/event_handler/message/mod.rs
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
+pub(super) mod message_delete;
+
 use poise::serenity_prelude::Context;
 use serenity::all::Message;
 
