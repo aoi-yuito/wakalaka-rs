8d4d03ba859945c13ecbf22371cef53356f5723f
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-19 20:05:24 +0200

    add utility functions to reduce duplication

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -13,6 +13,54 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
+use serenity::all::{GuildId, User, UserId};
+use tracing::{error, warn};
+
+use crate::Context;
+
 pub(super) mod buttons;
 pub(super) mod embeds;
 pub(super) mod messages;
+
+pub(crate) fn owner_id(ctx: Context<'_>) -> UserId {
+    let guild = match ctx.guild() {
+        Some(value) => value,
+        None => {
+            warn!("Couldn't get guild");
+            return UserId::new(0);
+        }
+    };
+    guild.owner_id
+}
+
+pub(crate) fn guild_name(ctx: Context<'_>) -> String {
+    let guild = match ctx.guild() {
+        Some(value) => value,
+        None => {
+            warn!("Couldn't get guild");
+            return String::new();
+        }
+    };
+    guild.name.clone()
+}
+
+pub(crate) fn guild_id(ctx: Context<'_>) -> GuildId {
+    let guild = match ctx.guild() {
+        Some(value) => value,
+        None => {
+            warn!("Couldn't get guild");
+            return GuildId::new(0);
+        }
+    };
+    guild.id
+}
+
+pub(crate) async fn user(user_id: UserId, ctx: Context<'_>) -> User {
+    match user_id.to_user(&ctx).await {
+        Ok(user) => user,
+        Err(why) => {
+            error!("Couldn't get user: {why:?}");
+            return User::default();
+        }
+    }
+}
