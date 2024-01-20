9e74fbfeee96e6c844ba7a1dbe5d27d5b840e329
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-20 21:47:39 +0200

    utility for user-related inquiries

---
+++ src/utility/users.rs
@@ -0,0 +1,29 @@
+// Copyright (C) 2024 Kawaxte
+//
+// wakalaka-rs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// wakalaka-rs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
+
+use serenity::all::{User, UserId};
+use tracing::error;
+
+use crate::Context;
+
+pub(crate) async fn user(ctx: Context<'_>, user_id: UserId) -> User {
+    match user_id.to_user(&ctx).await {
+        Ok(user) => user,
+        Err(why) => {
+            error!("Couldn't get user: {why:?}");
+            return User::default();
+        }
+    }
+}
