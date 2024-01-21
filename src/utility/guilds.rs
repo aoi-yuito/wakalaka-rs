5f38bd57a4c2f9ca7630c7c5c7add20ed9bdc988
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-21 11:43:08 +0200

    use `error!` when we have `Err(why)`

--- src/utility/guilds.rs
+++ src/utility/guilds.rs
@@ -14,7 +14,7 @@
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
 use serenity::all::{Guild, GuildChannel, GuildId, Member, UserId};
-use tracing::warn;
+use tracing::{error, warn};
 
 use crate::Context;
 
@@ -22,7 +22,7 @@ pub(crate) async fn channels(ctx: Context<'_>) -> Vec<GuildChannel> {
     match guild(ctx).await.channels(&ctx).await {
         Ok(channels) => channels.values().cloned().collect::<Vec<GuildChannel>>(),
         Err(why) => {
-            warn!("Couldn't get channels: {why:?}");
+            error!("Couldn't get channels: {why:?}");
             return Vec::new();
         }
     }
@@ -32,7 +32,7 @@ pub(crate) async fn member(ctx: Context<'_>, guild_id: GuildId, user_id: UserId)
     match guild_id.member(&ctx, user_id).await {
         Ok(member) => member,
         Err(why) => {
-            warn!("Couldn't get member: {why:?}");
+            error!("Couldn't get member: {why:?}");
             return Member::default();
         }
     }
@@ -42,7 +42,7 @@ pub(crate) async fn members_raw(ctx: &crate::serenity::Context, guild_id: GuildI
     match guild_id.members(&ctx, None, None).await {
         Ok(users) => users,
         Err(why) => {
-            warn!("Couldn't get members: {why:?}");
+            error!("Couldn't get members: {why:?}");
             return Vec::new();
         }
     }
@@ -52,7 +52,7 @@ pub(crate) async fn members(ctx: Context<'_>, guild_id: GuildId) -> Vec<Member> 
     match guild_id.members(&ctx, None, None).await {
         Ok(users) => users,
         Err(why) => {
-            warn!("Couldn't get members: {why:?}");
+            error!("Couldn't get members: {why:?}");
             return Vec::new();
         }
     }
