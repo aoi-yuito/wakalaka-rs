b663d1d61879b7ce026a1d0282c1a0e4141a3f4b
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-21 02:53:29 +0200

    add utility func for retrieving channels in guild

--- src/utility/guilds.rs
+++ src/utility/guilds.rs
@@ -13,10 +13,20 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
-use serenity::all::{Guild, GuildId, Member, UserId};
+use serenity::all::{Guild, GuildChannel, GuildId, Member, UserId};
 use tracing::warn;
 
 use crate::Context;
+
+pub(crate) async fn channels(ctx: Context<'_>) -> Vec<GuildChannel> {
+    match guild(ctx).await.channels(&ctx).await {
+        Ok(channels) => channels.values().cloned().collect::<Vec<GuildChannel>>(),
+        Err(why) => {
+            warn!("Couldn't get channels: {why:?}");
+            return Vec::new();
+        }
+    }
+}
 
 pub(crate) async fn member(ctx: Context<'_>, guild_id: GuildId, user_id: UserId) -> Member {
     match guild_id.member(&ctx, user_id).await {
