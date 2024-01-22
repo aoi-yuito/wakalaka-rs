0aa8ce20b50b2fba866e5b45ddc27eb8b76bec96
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-22 10:38:57 +0200

    rename `role` to `roles`

---
+++ src/framework/options/commands/manager/roles/mod.rs
@@ -0,0 +1,42 @@
+// Copyright (C) 2024 Kawaxte
+//
+// wakalaka-rs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// wakalaka-rs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
+
+mod add;
+mod delete;
+mod edit;
+mod list;
+mod remove;
+mod set;
+
+use crate::{
+    framework::commands::manager::roles::{
+        add::add, delete::delete, edit::edit, list::list, remove::remove, set::set,
+    },
+    Context, Error,
+};
+
+#[poise::command(
+    prefix_command,
+    slash_command,
+    subcommands("add", "delete", "edit", "list", "remove", "set"),
+    category = "Manager",
+    required_permissions = "MANAGE_ROLES",
+    guild_only,
+    subcommand_required,
+    ephemeral
+)]
+pub(crate) async fn roles(_: Context<'_>) -> Result<(), Error> {
+    Ok(())
+}
