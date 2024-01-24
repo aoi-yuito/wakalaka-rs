eb874c14a5f6e4ef8e9677c2c36a2aa6f535e841
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-24 13:44:01 +0200

    rename `emojsi` to `emoji`

--- src/framework/options/commands/manager/mod.rs
+++ src/framework/options/commands/manager/mod.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
-pub(super) mod emojis;
+pub(super) mod emoji;
 pub(super) mod nick;
 pub(super) mod purge;
 pub(super) mod roles;
