d30dca88e1d32834d9ba2cc00666a27292fbc5de
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-29 19:54:02 +0200

    use utility func for `guild_name`

--- src/framework/options/event_handler/guild/guild_member_removal.rs
+++ src/framework/options/event_handler/guild/guild_member_removal.rs
@@ -19,18 +19,15 @@ use tracing::info;
 use crate::{serenity::Context, utility::models};
 
 pub async fn handle(guild_id: &GuildId, user: &User, member: &Option<Member>, ctx: &Context) {
-    if let Some(member) = member {
-        let guild_id = member.guild_id;
-        let guild_name = models::guilds::guild_name_from_guild_id_raw(ctx, guild_id).await;
+    let guild_name = models::guilds::guild_name_from_guild_id_raw(ctx, *guild_id);
 
+    let user_name = &user.name;
+
+    if let Some(member) = member {
         let member_name = &member.user.name;
 
         info!("@{member_name} left from {guild_name}")
     } else {
-        let guild_name = models::guilds::guild_name_from_guild_id_raw(ctx, *guild_id).await;
-
-        let user_name = &user.name;
-
         info!("@{user_name} left from {guild_name}")
     }
 }
