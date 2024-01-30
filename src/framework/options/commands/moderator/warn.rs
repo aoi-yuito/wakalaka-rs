ffb76546c36abf9b34cf34afa4967192b1df4bf5
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-01-30 13:15:08 +0200

    formatting adjustment

--- src/framework/options/commands/moderator/warn.rs
+++ src/framework/options/commands/moderator/warn.rs
@@ -68,10 +68,8 @@ pub async fn warn(
 
     let reason_char_count = reason.chars().count();
     if reason_char_count < 6 || reason_char_count > 80 {
-        let reply = messages::info_reply(
-            "Reason must be between `6` and `80` characters long.",
-            true,
-        );
+        let reply =
+            messages::info_reply("Reason must be between `6` and `80` characters long.", true);
         if let Err(why) = ctx.send(reply).await {
             error!("Couldn't send reply: {why:?}");
             return Err(why.into());
