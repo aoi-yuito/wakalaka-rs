9a26507f38755dc015aa573d4cda779955abbe9c
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-11 18:28:50 +0200

    use `Result` in funct, warn if unavailable

--- src/framework/options/event_handler/guild/guild_delete.rs
+++ src/framework/options/event_handler/guild/guild_delete.rs
@@ -14,36 +14,40 @@
 // along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
 
 use serenity::all::{Guild, UnavailableGuild};
-use tracing::{error, info};
+use tracing::{error, info, warn};
 
-use crate::{database::guilds, utility::models, Data};
+use crate::{database::guilds, utility::models, Data, Error};
 
 pub async fn handle(
     unavailable_guild: &UnavailableGuild,
     guild: &Option<Guild>,
     ctx: &crate::serenity::Context,
     data: &Data,
-) {
-    if unavailable_guild.unavailable {
-        return;
-    }
-
+) -> Result<(), Error> {
     let pool = &data.pool;
 
     let unavailable_guild_id = unavailable_guild.id;
+    let unavailable_guild_name = &models::guilds::guild_name_raw(&ctx, unavailable_guild_id);
+
+    if unavailable_guild.unavailable {
+        warn!("{unavailable_guild_name} isn't available, skipping ...");
+        return Ok(());
+    }
 
     let guild = guild.as_ref().expect("Failed to get guild");
-    let guild_id = guild.id;
-    let guild_name = &guild.name;
+    let (guild_id, guild_name) = (guild.id, &guild.name);
 
     let combined_guild_ids = vec![guild_id, unavailable_guild_id];
     for combined_guild_id in combined_guild_ids {
         if let Err(why) = guilds::delete_from_guilds(&combined_guild_id, pool).await {
             error!("Failed to delete guild(s): {why:?}");
-        } else {
-            let app_name = models::current_application_name_raw(&ctx).await.unwrap();
-
-            info!("@{app_name} left from {guild_name}");
+            return Err(why.into());
         }
+
+        let bot_name = &ctx.cache.current_user().name;
+
+        info!("@{bot_name} left from {guild_name}");
     }
+
+    Ok(())
 }
