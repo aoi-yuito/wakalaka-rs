4ca97285ea804b82952605da3814c4ddec450026
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-15 22:36:41 +0200

    we don't need these

--- migrations/20240212192118_users.sql
+++ migrations/20240212192118_users.sql
@@ -1,6 +1,4 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id BIGINT PRIMARY KEY,
-    violations INTEGER NOT NULL 0,
-    timeout BOOLEAN DEFAULT FALSE,
-    ban BOOLEAN DEFAULT FALSE
+    violations INTEGER NOT NULL 0
 );
