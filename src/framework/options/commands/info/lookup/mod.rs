581ed684ca31396cf4cf40e883e25557b0e68397
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-16 05:05:07 +0200

    part of beta that actually works

--- src/framework/options/commands/info/lookup/mod.rs
+++ src/framework/options/commands/info/lookup/mod.rs
@@ -1,36 +1,21 @@
-// Copyright (C) 2024 Kawaxte
+// Copyright (c) 2024 Kawaxte
 //
-// wakalaka-rs is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// wakalaka-rs is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with wakalaka-rs. If not, see <http://www.gnu.org/licenses/>.
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
 
 mod server;
-mod user;
 
-use crate::{
-    framework::commands::info::lookup::{server::server, user::user},
-    Context, Error,
-};
+use crate::{framework::options::commands::info::lookup::server::server, Context, Error};
 
 #[poise::command(
-    prefix_command,
     slash_command,
-    subcommands("server", "user"),
-    category = "Core",
-    required_bot_permissions = "SEND_MESSAGES",
+    subcommands("server"),
+    category = "Information",
+    required_bot_permissions = "SEND_MESSAGES | EMBED_LINKS",
     guild_only,
     subcommand_required,
     ephemeral
 )]
-pub async fn lookup(_: Context<'_>) -> Result<(), Error> {
+pub(super) async fn lookup(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
