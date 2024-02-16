b71e5fdc60fed27d91c2cf528e645e718e4fc83d
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-16 04:44:29 +0200

    remove `owner_id`

--- migrations/20240212192137_restricted_guilds.sql
+++ migrations/20240212192137_restricted_guilds.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS restricted_guilds (
-    guild_id BIGINT PRIMARY KEY,
-    owner_id BIGINT NOT NULL,
+    guild_id BIGINT,
     reason VARCHAR(120) NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES users (user_id) ON DELETE CASCADE
+    PRIMARY KEY (guild_id)
 );
