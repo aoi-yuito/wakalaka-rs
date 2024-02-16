473e6996726216eaef18025e9e5aabff92e3d146
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-16 04:48:07 +0200

    `foreign key mismatch - \"violations\" referencing \"guilds\""` ???

--- migrations/20240212192156_violations.sql
+++ migrations/20240212192156_violations.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS violations (
-    uuid VARCHAR(32) PRIMARY KEY,
-    kind TEXT,
+    uuid UUID,
+    kind VARCHAR,
     guild_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
     moderator_id BIGINT NOT NULL,
     reason VARCHAR(120),
-    FOREIGN KEY (guild_id) REFERENCES guilds (guild_id),
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (uuid, kind),
+    FOREIGN KEY (guild_id) REFERENCES guilds (guild_id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users (user_id),
     FOREIGN KEY (moderator_id) REFERENCES users (user_id)
 );
