88c03d5f0c60e32d05e13681572929f9ad3edd8e
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-16 04:43:46 +0200

    minor adjustment and fix

--- migrations/20240212192118_users.sql
+++ migrations/20240212192118_users.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT PRIMARY KEY,
-    violations INTEGER NOT NULL 0
+    user_id BIGINT,
+    violations INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY (user_id)
 );
