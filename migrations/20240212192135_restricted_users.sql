2a27d799631991c2c5d8b65dcb5bfb3d2751ed37
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-16 04:44:17 +0200

    minor adjustment

--- migrations/20240212192135_restricted_users.sql
+++ migrations/20240212192135_restricted_users.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS restricted_users (
-    user_id BIGINT PRIMARY KEY,
-    reason VARCHAR(120) NOT NULL
+    user_id BIGINT,
+    reason VARCHAR(120) NOT NULL,
+    PRIMARY KEY (user_id)
 );
