28999d54373b21c07ab597af7d71c40e031cdb1b
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-02-20 00:12:26 +0200

    log when bot joins server

--- src/framework/options/event_handler/guild/guild_create.rs
+++ src/framework/options/event_handler/guild/guild_create.rs
@@ -5,9 +5,11 @@
 
 use serenity::all::Guild;
 use sqlx::SqlitePool;
+use tracing::info;
 
 use crate::{
     database::{checks, queries},
+    utils::models,
     Error, SContext,
 };
 
@@ -21,20 +23,23 @@ pub(crate) async fn handle(
         return Ok(());
     }
 
+    let bot = models::users::bot_raw(ctx).await?;
+    let bot_name = &bot.name;
+
     let guild_id = guild.id;
+    let guild_name = &guild.name;
 
     let guild_owner_id = guild.owner_id;
     let guild_owner = guild_owner_id.to_user(ctx).await?;
 
     let guild_restricted = checks::check_restricted_guild(ctx, db, guild, &guild_owner).await?;
     if guild_restricted {
-        guild.leave(ctx).await?;
         return Ok(());
     }
 
     queries::users::insert_into(db, &guild_owner_id).await?;
-
     queries::guilds::insert_into(db, &guild_id, &guild_owner_id).await?;
 
+    info!("@{bot_name} joined {guild_name}");
     Ok(())
 }
