7c67ef69fa830c37792b8362ae9bc0270b81cfd7
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-07 16:34:25 +0200

    message fix

--- src/framework/options/on_error/dm_only.rs
+++ src/framework/options/on_error/dm_only.rs
@@ -12,7 +12,7 @@ pub(crate) async fn handle(ctx: Context<'_>) {
     let command_name = &command.name;
 
     let reply = components::replies::error_reply_embed(
-        format!("Cannot `{command_name}` outside DMs."),
+        format!("Cannot invoke `{command_name}` outside DMs."),
         true,
     );
 
