b091fbdc9bab387e35671564abfd3bcb9d386a3a
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-07 16:26:37 +0200

    if manually deleted, remove entry from `restricted_users`

--- migrations/20240212192137_restricted_guilds.sql
+++ migrations/20240212192137_restricted_guilds.sql
@@ -3,3 +3,8 @@ CREATE TABLE IF NOT EXISTS restricted_guilds (
     reason VARCHAR(255) NOT NULL,
     PRIMARY KEY (guild_id)
 );
+CREATE TRIGGER IF NOT EXISTS restricted_guilds_delete
+AFTER DELETE ON restricted_guilds BEGIN
+DELETE FROM restricted_users
+WHERE guild_id = OLD.guild_id;
+END;
