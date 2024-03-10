3c9291d900a77cbf06efdff5713f0132fe7f8114
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-10 05:16:56 +0200

    store Last.fm session

--- migrations/20240212192118_users.sql
+++ migrations/20240212192118_users.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id BIGINT,
+    lastfm_name VARCHAR(15),
+    lastfm_key VARCHAR(32),
     violations INTEGER NOT NULL DEFAULT 0,
     PRIMARY KEY (user_id)
 );
