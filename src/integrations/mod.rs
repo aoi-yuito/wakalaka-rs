18882eca5f89d6c40afff38c089b9630be3dc87b
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-10 05:30:56 +0200

    should use our app as `User-Agent`

--- src/integrations/mod.rs
+++ src/integrations/mod.rs
@@ -3,6 +3,11 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+const APP_USER_AGENT: &str = concat!(
+    std::env!("CARGO_PKG_NAME"),
+    "/",
+    std::env!("CARGO_PKG_VERSION")
+);
 const POSTMAN_USER_AGENT: &str = "PostmanRuntime/7.36.0";
 
 pub(crate) mod lastfm;
