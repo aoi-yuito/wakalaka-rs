19cc7a6519a4fa42a35cdb22869914d95c1ba94f
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-26 16:28:04 +0200

    barely anybody uses Last.fm

--- migrations/20240212192118_users.sql
+++ migrations/20240212192118_users.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id BIGINT,
-    lastfm_name VARCHAR(15),
-    lastfm_key VARCHAR(32),
     violations INTEGER NOT NULL DEFAULT 0,
     PRIMARY KEY (user_id)
 );
