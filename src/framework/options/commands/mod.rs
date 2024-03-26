583ae291760fe85031e97a4c83d2e05b5e262c20
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-26 16:29:11 +0200

    register internally instead

--- src/framework/options/commands/mod.rs
+++ src/framework/options/commands/mod.rs
@@ -10,21 +10,47 @@ mod integrations;
 mod manager;
 mod misc;
 mod moderator;
-mod music;
 
 use poise::Command;
+use serenity::all::GuildId;
+use tracing::{error, info, warn};
 
-use crate::{Data, Error};
+use crate::{Data, Error, SContext, Throwable};
+
+pub(crate) async fn register_guild_commands(ctx: &SContext, guild_id: &GuildId) -> Throwable<()> {
+    let commands = commands().await;
+
+    let command_count = commands.len();
+    if command_count == 0 {
+        warn!("No commands to register");
+
+        return Ok(());
+    }
+
+    let message = if command_count < 1 {
+        format!("Registered {command_count} command")
+    } else {
+        format!("Registered {command_count} commands")
+    };
+
+    if let Err(why) = poise::builtins::register_in_guild(ctx, &commands, *guild_id).await {
+        error!("Failed to register in guild: {why}");
+        return Err(why.into());
+    }
+
+    info!("{message}");
+
+    Ok(())
+}
 
 pub(crate) async fn commands() -> Vec<Command<Data, Error>> {
     let mut commands = vec![];
     commands.extend(core::commands().await);
     commands.extend(fun::commands().await);
     commands.extend(info::commands().await);
-    commands.extend(integrations::commands().await);
+    // commands.extend(integrations::commands().await);
     commands.extend(manager::commands().await);
     commands.extend(misc::commands().await);
     commands.extend(moderator::commands().await);
-    commands.extend(music::commands().await);
     commands
 }
--- src/framework/options/event_handler/ready/cache_ready.rs
+++ src/framework/options/event_handler/ready/cache_ready.rs
@@ -6,14 +6,18 @@
 use serenity::all::GuildId;
 use tracing::info;
 
-use crate::Throwable;
+use crate::{framework::options, SContext, Throwable};
 
-pub(crate) async fn handle(guild_ids: &Vec<GuildId>) -> Throwable<()> {
+pub(crate) async fn handle(ctx: &SContext, guild_ids: &Vec<GuildId>) -> Throwable<()> {
     let guild_id_count = guild_ids.len();
     if guild_id_count == 1 {
         info!("Readied cache for 1 server");
     } else {
         info!("Readied cache for {guild_id_count} servers");
+    }
+
+    for guild_id in guild_ids {
+        options::commands::register_guild_commands(ctx, guild_id).await?;
     }
 
     Ok(())
