7e6db3e3a1a226ca0cb3bb64735fb101ce477bf2
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-31 19:09:42 +0300

    store all `type`s in their own space

---
+++ wakalaka-core/src/types.rs
@@ -0,0 +1,20 @@
+// Copyright (c) 2024 Kawaxte
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+use poise::serenity_prelude as serenity;
+
+use crate::data::Data;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type FrameworkError<'a> = poise::FrameworkError<'a, Data, Error>;
+
+pub type Throwable<T> = Result<T, Error>;
+
+pub type Context<'a> = poise::Context<'a, Data, Error>;
+pub type FrameworkContext<'a> = poise::FrameworkContext<'a, Data, Error>;
+
+pub type SClient = serenity::Client;
+pub type SContext = serenity::Context;
+pub type SReady = serenity::Ready;
