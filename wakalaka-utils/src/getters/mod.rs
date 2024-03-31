704aa1a556bb3d5ac274fa29b4b8dab4c7b70434
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-03-31 22:00:18 +0300

    rename to `getters` due to methods returning values

---
+++ wakalaka-utils/src/getters/mod.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Kawaxte
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+pub mod channels;
+pub mod emojis;
+pub mod guilds;
+pub mod roles;
+pub mod users;
