c3c39873dcd557716fa0bbca8b6548db9e0b9a0e
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-07 09:27:15 +0300

    separate to own file

---
+++ wakalaka-core/src/constants.rs
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Kawaxte
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+pub const CARGO_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const CARGO_NAME: &str = env!("CARGO_PKG_NAME");
+pub const CARGO_AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+pub const CARGO_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+pub const CARGO_REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
+
+pub const GITHUB_URL: &str = "https://github.com/Kawaxte";
+
+pub const INVITE_URL: &str = "https://discord.gg/jUZVWk7q2q";
+pub const BOT_INVITE_URL: &str = "https://discord.com/api/oauth2/authorize?client_id=1190718691055251548&permissions=9925535296631&scope=bot";
