b2575766bfdf41a95d0faab2e2c7f613cf0333ba
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-07 20:16:51 +0300

    finalise `core` module

--- wakalaka-core/src/lib.rs
+++ wakalaka-core/src/lib.rs
@@ -5,10 +5,47 @@
 
 pub mod consts;
 pub mod envs;
+pub mod options;
 pub mod types;
+pub mod checks;
 
+use poise::Framework;
+use serenity::all::GatewayIntents;
 use sqlx::SqlitePool;
+use tracing::subscriber;
+use types::{Error, SContext, SReady, Throwable};
 
 pub struct Data {
     pub db: SqlitePool,
+}
+
+pub async fn fetch_gateway_intents() -> GatewayIntents {
+    GatewayIntents::non_privileged()
+        | GatewayIntents::GUILDS
+        | GatewayIntents::GUILD_MEMBERS
+        | GatewayIntents::GUILD_MODERATION
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT
+}
+
+pub async fn fetch_user_data(
+    _ctx: &SContext,
+    _ready: &SReady,
+    _framework: &Framework<Data, Error>,
+    data: Data,
+) -> Throwable<Data> {
+    Ok(data)
+}
+
+pub async fn build_subscriber() -> Throwable<()> {
+    let rust_log = envs::fetch_rust_log_from_env()?;
+
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter(rust_log)
+        .compact()
+        .finish();
+    subscriber::set_global_default(subscriber)?;
+
+    Ok(())
 }
