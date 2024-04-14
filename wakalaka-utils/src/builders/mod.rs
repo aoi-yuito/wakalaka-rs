d4128fa79a88645507c36f9006e7ed270d21b7bd
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-14 21:45:40 +0300

    re-impl serenity builders

---
+++ wakalaka-utils/src/builders/mod.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 Kawaxte
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+pub mod embeds;
+pub mod messages;
+pub mod replies;
+pub mod responses;
