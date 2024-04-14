2f53993edd7602ebd947c2cdc6891b0e9819d419
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-14 21:42:44 +0300

    re-impl command restriction checker

---
+++ src/options/command_check.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2024 Kawaxte
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+use wakalaka_core::types::{Context, Throwable};
+
+use crate::database;
+
+pub(super) async fn handle_command_check_option(ctx: Context<'_>) -> Throwable<bool> {
+    let db = &ctx.data().db;
+
+    let user = ctx.author();
+    let user_id = &user.id;
+
+    let user_restricted = database::checks::is_user_restricted(db, ctx, user_id).await?;
+    if user_restricted {
+        Ok(false)
+    } else {
+        Ok(true)
+    }
+}
