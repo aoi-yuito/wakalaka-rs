00005832cdb1b50a1ae8b2fda736197d53303f63
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-14 21:38:46 +0300

    relocate to `builders` module

--- wakalaka-utils/src/factories/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 Kawaxte
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-pub mod embeds;
-pub mod messages;
-pub mod replies;
-pub mod responses;
