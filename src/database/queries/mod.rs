f2b84c4ffec054253dcf13adcdebc410b6f6d7ac
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-14 21:41:37 +0300

    visibility changes

--- src/database/queries/mod.rs
+++ src/database/queries/mod.rs
@@ -1,10 +1,10 @@
 // Copyright (c) 2024 Kawaxte
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-pub mod guilds;
-pub mod infractions;
-pub mod restricted_guilds;
-pub mod restricted_users;
-pub mod users;
+pub(crate) mod guilds;
+pub(crate) mod infractions;
+pub(crate) mod restricted_guilds;
+pub(crate) mod restricted_users;
+pub(crate) mod users;
