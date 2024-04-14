7217360a739f49cbb59d0c5a45ab8241bac669f9
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-14 21:45:07 +0300

    fetch both non-raw and raw role

--- wakalaka-utils/src/accessors/roles.rs
+++ wakalaka-utils/src/accessors/roles.rs
@@ -4,16 +4,30 @@
 // https://opensource.org/licenses/MIT
 
 use serenity::all::{GuildId, ModelError, Role, RoleId};
-use wakalaka_core::types::{Context, Throwable};
+use wakalaka_core::types::{Context, SContext, Throwable};
 
 use super::guilds;
 
-pub async fn retrieve_specific_guild_role(
+pub async fn fetch_role(
     ctx: Context<'_>,
     guild_id: &GuildId,
     role_id: &RoleId,
 ) -> Throwable<Role> {
     let guild = guilds::fetch_cached_guild(ctx, guild_id)?;
+
+    let role = guild
+        .roles
+        .get(&role_id)
+        .ok_or_else(|| Box::new(ModelError::RoleNotFound))?;
+    Ok(role.clone())
+}
+
+pub async fn fetch_raw_role(
+    ctx: &SContext,
+    guild_id: &GuildId,
+    role_id: &RoleId,
+) -> Throwable<Role> {
+    let guild = guilds::fetch_raw_cached_guild(ctx, guild_id)?;
 
     let role = guild
         .roles
