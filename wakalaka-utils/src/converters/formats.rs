969fc9dfbc5b3681f067f590e562cc962a60c4b2
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-19 23:57:30 +0300

    less cluttered conversion

--- wakalaka-utils/src/converters/formats.rs
+++ wakalaka-utils/src/converters/formats.rs
@@ -7,38 +7,28 @@ use regex::Regex;
 use wakalaka_core::types::Throwable;
 
 pub fn html_to_discord_md(mut input: String) -> Throwable<String> {
-    let a_href_re = Regex::new(r#"<a href="(.*?)">(.*?)</a>"#)?;
-    let b_re = Regex::new(r"<b>(.*?)</b>")?;
-    let blockquote_re = Regex::new(r"<blockquote>(.*?)</blockquote>")?;
-    let br_re = Regex::new(r"<br>")?;
-    let code_re = Regex::new(r"<code>(.*?)</code>")?;
-    let dd_re = Regex::new(r"<dd>(.*?)</dd>")?;
-    let dt_re = Regex::new(r"<dt>(.*?)</dt>")?;
-    let h1_re = Regex::new(r"<h1>(.*?)</h1>")?;
-    let h2_re = Regex::new(r"<h2>(.*?)</h2>")?;
-    let h3_re = Regex::new(r"<h3>(.*?)</h3>")?;
-    let hr_re = Regex::new(r"<hr>")?;
-    let i_re = Regex::new(r"<i>(.*?)</i>")?;
-    let li = Regex::new(r"<li>(.*?)</li>")?;
-    let ol = Regex::new(r"<ol>(.*?)</ol>")?;
-    let u_re = Regex::new(r"<u>(.*?)</u>")?;
-    let s_re = Regex::new(r"<s>(.*?)</s>")?;
+    let replacements = [
+        (Regex::new(r#"<a href="(.*?)">(.*?)</a>"#)?, "[$2]($1)"),
+        (Regex::new(r"<b>(.*?)</b>")?, "**$1**"),
+        (Regex::new(r"<blockquote>(.*?)</blockquote>")?, "> $1"),
+        (Regex::new(r"<br>")?, "\n"),
+        (Regex::new(r"<code>(.*?)</code>")?, "`$1`"),
+        (Regex::new(r"<dd>(.*?)</dd>")?, "$1"),
+        (Regex::new(r"<dt>(.*?)</dt>")?, "\t$1"),
+        (Regex::new(r"<hr>")?, "---"),
+        (Regex::new(r"<h1>(.*?)</h1>")?, "# $1"),
+        (Regex::new(r"<h2>(.*?)</h2>")?, "## $1"),
+        (Regex::new(r"<h3>(.*?)</h3>")?, "### $1"),
+        (Regex::new(r"<i>(.*?)</i>")?, "*$1*"),
+        (Regex::new(r"<li>(.*?)</li>")?, "- $1"),
+        (Regex::new(r"<ol>(.*?)</ol>")?, "1. $1"),
+        (Regex::new(r"<u>(.*?)</u>")?, "__$1__"),
+        (Regex::new(r"<s>(.*?)</s>")?, "~~$1~~"),
+    ];
 
-    input = format!("{}", a_href_re.replace_all(&input, "[$2]($1)"));
-    input = format!("{}", b_re.replace_all(&input, "**$1**"));
-    input = format!("{}", blockquote_re.replace_all(&input, "> $1"));
-    input = format!("{}", br_re.replace_all(&input, "\n"));
-    input = format!("{}", code_re.replace_all(&input, "`$1`"));
-    input = format!("{}", dd_re.replace_all(&input, "$1"));
-    input = format!("{}", dt_re.replace_all(&input, "\t$1"));
-    input = format!("{}", hr_re.replace_all(&input, "---"));
-    input = format!("{}", h1_re.replace_all(&input, "# $1"));
-    input = format!("{}", h2_re.replace_all(&input, "## $1"));
-    input = format!("{}", h3_re.replace_all(&input, "### $1"));
-    input = format!("{}", i_re.replace_all(&input, "*$1*"));
-    input = format!("{}", li.replace_all(&input, "- $1"));
-    input = format!("{}", ol.replace_all(&input, "1. $1"));
-    input = format!("{}", u_re.replace_all(&input, "__$1__"));
-    input = format!("{}", s_re.replace_all(&input, "~~$1~~"));
+    for (re, replacement) in replacements {
+        input = format!("{}", re.replace_all(&input, replacement));
+    }
+
     Ok(input)
 }
