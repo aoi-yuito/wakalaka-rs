9e0db2b63dc1f098473814a03276240affd0830d
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-21 14:17:31 +0300

    `why` to `e`

--- wakalaka-core/src/envs.rs
+++ wakalaka-core/src/envs.rs
@@ -8,25 +8,25 @@ use tracing::error;
 use super::types::Throwable;
 
 pub fn fetch_database_url_from_env() -> Throwable<String> {
-    let database_url = dotenvy::var("DATABASE_URL").map_err(|why| {
-        error!("Failed to find DATABASE_URL in environment: {why:?}");
-        why
+    let database_url = dotenvy::var("DATABASE_URL").map_err(|e| {
+        error!("Failed to find DATABASE_URL in environment: {e:?}");
+        e
     })?;
     Ok(database_url)
 }
 
 pub fn fetch_discord_token_from_env() -> Throwable<String> {
-    let discord_token = dotenvy::var("DISCORD_TOKEN").map_err(|why| {
-        error!("Failed to find DISCORD_TOKEN in environment: {why:?}");
-        why
+    let discord_token = dotenvy::var("DISCORD_TOKEN").map_err(|e| {
+        error!("Failed to find DISCORD_TOKEN in environment: {e:?}");
+        e
     })?;
     Ok(discord_token)
 }
 
 pub fn fetch_rust_log_from_env() -> Throwable<String> {
-    let rust_log = dotenvy::var("RUST_LOG").map_err(|why| {
-        error!("Failed to find RUST_LOG in environment: {why:?}");
-        why
+    let rust_log = dotenvy::var("RUST_LOG").map_err(|e| {
+        error!("Failed to find RUST_LOG in environment: {e:?}");
+        e
     })?;
     Ok(rust_log)
 }
