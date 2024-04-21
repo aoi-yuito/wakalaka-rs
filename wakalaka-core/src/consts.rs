b5ac89b50c368edbe5667386725cd51c8b9ee51f
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-21 22:23:58 +0300

    move `CARGO` consts to main crate

--- wakalaka-core/src/consts.rs
+++ wakalaka-core/src/consts.rs
@@ -3,13 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-pub const CARGO_VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const CARGO_NAME: &str = env!("CARGO_PKG_NAME");
-pub const CARGO_AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
-pub const CARGO_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
-pub const CARGO_REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
+pub const DEV_GITHUB_URL: &str = "https://github.com/Kawaxte";
 
-pub const GITHUB_URL: &str = "https://github.com/Kawaxte";
-
-pub const INVITE_URL: &str = "https://discord.gg/jUZVWk7q2q";
-pub const BOT_INVITE_URL: &str = "https://discord.com/api/oauth2/authorize?client_id=1190718691055251548&permissions=9925535296631&scope=bot";
+pub const APP_INVITE_URL: &str = "https://discord.com/api/oauth2/authorize?client_id=1190718691055251548&permissions=9925535296631&scope=bot";
+pub const SERVER_INVITE_URL: &str = "https://discord.gg/jUZVWk7q2q";
