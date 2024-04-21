35ec2c64417eba75bf5701bcbb92589bdbf5304a
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-21 21:19:59 +0300

    acknowledge component interacts

--- src/events/interaction/interaction_create.rs
+++ src/events/interaction/interaction_create.rs
@@ -3,7 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-use serenity::all::Interaction;
+use serenity::all::{CreateInteractionResponse, Interaction};
 use tracing::{error, info};
 use wakalaka_core::types::{SContext, Throwable};
 use wakalaka_utils::accessors;
@@ -12,7 +12,6 @@ pub(crate) async fn handle_interaction_create_event(
     ctx: &SContext,
     interact: &Interaction,
 ) -> Throwable<()> {
-    //check if the interaction is a command
     match interact {
         Interaction::Command(interact) => {
             let user = &interact.user;
@@ -21,10 +20,15 @@ pub(crate) async fn handle_interaction_create_event(
             let cmd_data = &interact.data;
             let cmd_name = &cmd_data.name;
 
-            let guild_id = &interact.guild_id.expect("Failed to find guild ID.");
+            let guild_id = &interact.guild_id.expect("Guild ID not found");
             let guild_name = accessors::guilds::fetch_raw_guild_name(ctx, guild_id);
 
             info!("@{user_name} executed /{cmd_name} in {guild_name}");
+        }
+        Interaction::Component(interact) => {
+            interact
+                .create_response(ctx, CreateInteractionResponse::Acknowledge)
+                .await?;
         }
         _ => {
             error!("Unhandled interaction: {interact:?}");
