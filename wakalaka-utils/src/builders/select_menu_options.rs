4a00d1bdcf0dca0efd208bdf67d77e3383555c0d
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-21 21:21:03 +0300

    selection for guilds in restriction

--- wakalaka-utils/src/builders/select_menu_options.rs
+++ wakalaka-utils/src/builders/select_menu_options.rs
@@ -3,7 +3,23 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-use serenity::all::{CreateSelectMenuOption, ReactionType};
+use serenity::all::{CreateSelectMenuOption, GuildId, ReactionType, UserId};
+use sqlx::types::chrono::NaiveDateTime;
+
+pub async fn build_restricted_guild_select_menu_option(
+    guild_id: &GuildId,
+    reason: &String,
+    created_at: &NaiveDateTime,
+) -> CreateSelectMenuOption {
+    let simple_created_at = format!("{}", created_at.format("%b %d, %Y"));
+
+    CreateSelectMenuOption::new(
+        format!("{guild_id} ({simple_created_at})"),
+        format!("{guild_id}"),
+    )
+    .description(reason.trim())
+    .emoji(ReactionType::Unicode(format!("⛔")))
+}
 
 pub fn build_select_menu_option_with_emoji(
     label: impl Into<String>,
