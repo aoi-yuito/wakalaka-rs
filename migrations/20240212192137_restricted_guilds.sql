85b5f5742e5f95afa78a8317204b65509bba5af9
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-21 21:19:09 +0300

    can't find `guild_id` as col

--- migrations/20240212192137_restricted_guilds.sql
+++ migrations/20240212192137_restricted_guilds.sql
@@ -4,8 +4,3 @@ CREATE TABLE IF NOT EXISTS restricted_guilds (
     created_at TIMESTAMP NOT NULL,
     PRIMARY KEY (guild_id)
 );
-CREATE TRIGGER IF NOT EXISTS restricted_guilds_delete
-AFTER DELETE ON restricted_guilds BEGIN
-DELETE FROM restricted_users
-WHERE guild_id = OLD.guild_id;
-END;
