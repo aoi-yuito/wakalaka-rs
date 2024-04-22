b58900db51ba1289356e26fc5473cc207b78cbf2
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-23 00:19:04 +0300

    reply msg adjustment

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -30,24 +30,20 @@ pub(crate) async fn gather_all_commands() -> Vec<Command> {
     commands.extend(fun::commands().await);
     commands.extend(info::commands().await);
     commands.extend(manager::commands().await);
+    commands.extend(moderator::commands().await);
     commands
 }
 
 async fn is_user_bot_or_system(ctx: Context<'_>, user: &User) -> Throwable<bool> {
     if user.bot {
-        let reply = builders::replies::build_error_reply_with_embed(
-            "Cannot restrict bots from using yours truly.",
-            true,
-        );
+        let reply = builders::replies::build_error_reply_with_embed("Cannot act on a bot.", true);
 
         ctx.send(reply).await?;
 
         return Ok(true);
     } else if user.system {
-        let reply = builders::replies::build_error_reply_with_embed(
-            "Cannot restrict system users from using yours truly.",
-            true,
-        );
+        let reply =
+            builders::replies::build_error_reply_with_embed("Cannot act on a system user.", true);
 
         ctx.send(reply).await?;
 
