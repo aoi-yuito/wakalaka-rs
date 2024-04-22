2c2742e2297145867743e5702d5f121fcdc0c225
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-22 18:09:20 +0300

    Discord things for emojis and stickers

--- wakalaka-core/src/consts.rs
+++ wakalaka-core/src/consts.rs
@@ -7,3 +7,9 @@ pub const DEV_GITHUB_URL: &str = "https://github.com/Kawaxte";
 
 pub const APP_INVITE_URL: &str = "https://discord.com/api/oauth2/authorize?client_id=1190718691055251548&permissions=9925535296631&scope=bot";
 pub const SERVER_INVITE_URL: &str = "https://discord.gg/jUZVWk7q2q";
+
+pub const STICKER_MAX_SIZE: u32 = 1024 * 512; // 512 KB
+pub const STICKER_MAX_DIMENSIONS: (u32, u32) = (320, 320);
+pub const STICKER_EXTENSIONS: [&str; 3] = ["apng", "gif", "png"];
+
+pub const EMOJI_EXTENSIONS: [&str; 4] = ["gif", "jpeg", "jpg", "png"];
