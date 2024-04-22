825d35289288cf397f7019d5f55b87ea089fc249
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-23 01:14:46 +0300

    forgot to change link

--- wakalaka-core/src/consts.rs
+++ wakalaka-core/src/consts.rs
@@ -5,7 +5,8 @@
 
 pub const DEV_GITHUB_URL: &str = "https://github.com/Kawaxte";
 
-pub const APP_INVITE_URL: &str = "https://discord.com/api/oauth2/authorize?client_id=1190718691055251548&permissions=9925535296631&scope=bot";
+pub const APP_INVITE_URL: &str =
+    "https://discord.com/oauth2/authorize?client_id=1190718691055251548";
 pub const SERVER_INVITE_URL: &str = "https://discord.gg/jUZVWk7q2q";
 
 pub const STICKER_MAX_SIZE: u32 = 1024 * 512; // 512 KB
