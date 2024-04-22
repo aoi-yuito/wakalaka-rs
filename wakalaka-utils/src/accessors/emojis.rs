2d62d7fc166b19759cc94883f778d0a7d87a0e33
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-22 03:04:29 +0300

    remove unused

--- wakalaka-utils/src/accessors/emojis.rs
+++ wakalaka-utils/src/accessors/emojis.rs
@@ -3,7 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-use serenity::all::{Emoji, EmojiId};
+use serenity::all::Emoji;
 use wakalaka_core::types::{Context, Throwable};
 
 use super::guilds;
@@ -13,11 +13,4 @@ pub async fn gather_all_guild_emojis(ctx: Context<'_>) -> Throwable<Vec<Emoji>> 
 
     let emojis = guild.emojis(ctx).await?;
     Ok(emojis)
-}
-
-pub async fn fetch_guild_emoji(ctx: Context<'_>, emoji_id: &EmojiId) -> Throwable<Emoji> {
-    let guild = guilds::fetch_guild(ctx)?;
-
-    let emoji = guild.emoji(ctx, *emoji_id).await?;
-    Ok(emoji)
 }
