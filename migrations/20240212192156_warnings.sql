bcd00f732903cf7594c427d332e912e1b66bd71f
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-25 01:32:21 +0300

    `reason` should not be nullable

--- migrations/20240212192156_warnings.sql
+++ migrations/20240212192156_warnings.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS warnings (
     guild_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
     moderator_id BIGINT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
-    reason VARCHAR(255),
+    reason VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     PRIMARY KEY (uuid),
     FOREIGN KEY (guild_id) REFERENCES guilds (guild_id) ON DELETE CASCADE
