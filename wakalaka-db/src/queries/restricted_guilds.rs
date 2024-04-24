759611c54dc2b2c74ac13a439f1d3a17b5bd408e
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-04-25 01:33:05 +0300

    log msg adjustments, use `query!()` over `query()`

--- wakalaka-db/src/queries/restricted_guilds.rs
+++ wakalaka-db/src/queries/restricted_guilds.rs
@@ -4,25 +4,23 @@
 // https://opensource.org/licenses/MIT
 
 use serenity::all::{GuildId, Timestamp};
-use sqlx::{
-    types::chrono::{DateTime, NaiveDateTime},
-    PgPool, Row,
-};
+use sqlx::{types::chrono::NaiveDateTime, PgPool};
 
 use wakalaka_core::types::SqlxThrowable;
+use wakalaka_utils::converters::times;
 
 pub async fn gather_all_restricted_guilds_from_db(
     pool: &PgPool,
 ) -> SqlxThrowable<Vec<(GuildId, String, NaiveDateTime)>> {
-    let select = sqlx::query("SELECT * FROM restricted_guilds");
+    let select = sqlx::query!("SELECT guild_id, reason, created_at FROM restricted_guilds");
 
     let mut restricted_guilds = vec![];
 
     let rows = select.fetch_all(pool).await?;
     for row in rows {
-        let guild_id = GuildId::from(row.get::<i64, _>("guild_id") as u64);
-        let reason = row.get::<String, _>("reason");
-        let created_at = row.get::<NaiveDateTime, _>("created_at");
+        let guild_id = GuildId::from(row.guild_id as u64);
+        let reason = row.reason.clone();
+        let created_at = row.created_at;
 
         restricted_guilds.push((guild_id, reason, created_at));
     }
@@ -34,32 +32,38 @@ pub async fn fetch_created_at_from_db(
     pool: &PgPool,
     guild_id: &GuildId,
 ) -> SqlxThrowable<NaiveDateTime> {
-    let select = sqlx::query("SELECT created_at FROM restricted_guilds WHERE guild_id = $1")
-        .bind(i64::from(*guild_id));
+    let select = sqlx::query!(
+        "SELECT created_at FROM restricted_guilds WHERE guild_id = $1",
+        i64::from(*guild_id)
+    );
 
     let row = select.fetch_one(pool).await?;
 
-    let created_at = row.get::<NaiveDateTime, _>("created_at");
+    let created_at = row.created_at;
     Ok(created_at)
 }
 
 pub async fn fetch_reason_from_db(pool: &PgPool, guild_id: &GuildId) -> SqlxThrowable<String> {
-    let select = sqlx::query("SELECT reason FROM restricted_guilds WHERE guild_id = $1")
-        .bind(i64::from(*guild_id));
+    let select = sqlx::query!(
+        "SELECT reason FROM restricted_guilds WHERE guild_id = $1",
+        i64::from(*guild_id)
+    );
 
     let row = select.fetch_one(pool).await?;
 
-    let reason = row.get::<String, _>("reason");
+    let reason = row.reason;
     Ok(reason)
 }
 
 pub async fn fetch_guild_id_from_db(pool: &PgPool, guild_id: &GuildId) -> SqlxThrowable<GuildId> {
-    let query = sqlx::query("SELECT guild_id FROM restricted_guilds WHERE guild_id = $1")
-        .bind(i64::from(*guild_id));
+    let select = sqlx::query!(
+        "SELECT guild_id FROM restricted_guilds WHERE guild_id = $1",
+        i64::from(*guild_id)
+    );
 
-    let row = query.fetch_one(pool).await?;
+    let row = select.fetch_one(pool).await?;
 
-    let guild_id = GuildId::from(row.get::<i64, _>("guild_id") as u64);
+    let guild_id = GuildId::from(row.guild_id as u64);
     Ok(guild_id)
 }
 
@@ -69,11 +73,13 @@ pub async fn remove_restricted_guild_from_db(
 ) -> SqlxThrowable<()> {
     let transaction = pool.begin().await?;
 
-    let delete = sqlx::query("DELETE FROM restricted_guilds WHERE guild_id = $1")
-        .bind(i64::from(*guild_id))
-        .execute(pool);
+    let delete = sqlx::query!(
+        "DELETE FROM restricted_guilds WHERE guild_id = $1",
+        i64::from(*guild_id)
+    )
+    .execute(pool);
     if let Err(e) = delete.await {
-        tracing::error!("Failed to remove restricted guild from database: {e:?}");
+        tracing::error!("Failed to DELETE FROM restricted_guilds: {e:?}");
 
         transaction.rollback().await?;
 
@@ -93,15 +99,14 @@ pub async fn add_restricted_guild_to_db(
 ) -> SqlxThrowable<()> {
     let transaction = pool.begin().await?;
 
-    let insert = sqlx::query(
+    let insert = sqlx::query!(
         "INSERT INTO restricted_guilds (guild_id, reason, created_at) VALUES ($1, $2, $3) ON CONFLICT (guild_id) DO NOTHING",
-    )
-    .bind(i64::from(*guild_id))
-    .bind(reason.trim())
-    .bind(DateTime::from_timestamp(created_at.timestamp(), 0))
-    .execute(pool);
+        i64::from(*guild_id),
+        reason.trim(),
+        times::datetime_to_naivedatetime(created_at)
+    ).execute(pool);
     if let Err(e) = insert.await {
-        tracing::error!("Failed to add restricted guild to database: {e:?}");
+        tracing::error!("Failed to INSERT INTO restricted_guilds: {e:?}");
 
         transaction.rollback().await?;
 
--- wakalaka-db/src/queries/warnings.rs
+++ wakalaka-db/src/queries/warnings.rs
@@ -6,40 +6,22 @@
 use serenity::all::{GuildId, UserId};
 use sqlx::{
     types::chrono::{NaiveDateTime, Utc},
-    PgPool, Row,
+    PgPool,
 };
 
 use uuid::Uuid;
 use wakalaka_core::types::SqlxThrowable;
-
-pub async fn gather_all_uuids_from_db(
-    pool: &PgPool,
-    guild_id: &GuildId,
-    user_id: &UserId,
-) -> SqlxThrowable<Vec<Uuid>> {
-    let select = sqlx::query("SELECT uuid FROM warnings WHERE guild_id = $1 AND user_id = $2")
-        .bind(i64::from(*guild_id))
-        .bind(i64::from(*user_id));
-
-    let rows = select.fetch_all(pool).await?;
-
-    let uuids = rows
-        .iter()
-        .map(|row| Uuid::parse_str(&row.get::<String, _>("uuid")).expect("Failed to parse UUID"))
-        .collect();
-    Ok(uuids)
-}
 
 pub async fn gather_all_from_db(
     pool: &PgPool,
     guild_id: &GuildId,
     user_id: &UserId,
 ) -> SqlxThrowable<Vec<(Uuid, UserId, UserId, String, NaiveDateTime)>> {
-    let select = sqlx::query(
+    let select = sqlx::query!(
         "SELECT uuid, user_id, moderator_id, reason, created_at FROM warnings WHERE guild_id = $1 AND user_id = $2",
-    )
-    .bind(i64::from(*guild_id))
-    .bind(i64::from(*user_id));
+        i64::from(*guild_id),
+        i64::from(*user_id)
+    );
 
     let rows = select.fetch_all(pool).await?;
 
@@ -47,25 +29,47 @@ pub async fn gather_all_from_db(
         .iter()
         .map(|row| {
             (
-                Uuid::parse_str(&row.get::<String, _>("uuid")).expect("Failed to parse UUID"),
-                UserId::new(row.get::<i64, _>("user_id") as u64),
-                UserId::new(row.get::<i64, _>("moderator_id") as u64),
-                row.get::<String, _>("reason"),
-                row.get::<NaiveDateTime, _>("created_at"),
+                Uuid::parse_str(&row.uuid).expect("Failed to parse UUID"),
+                UserId::new(row.user_id as u64),
+                UserId::new(row.moderator_id as u64),
+                row.reason.clone(),
+                row.created_at,
             )
         })
-        .collect();
+        .collect::<Vec<_>>();
     Ok(warnings)
+}
+
+pub async fn gather_all_uuids_from_db(
+    pool: &PgPool,
+    guild_id: &GuildId,
+    user_id: &UserId,
+) -> SqlxThrowable<Vec<Uuid>> {
+    let select = sqlx::query!(
+        "SELECT uuid FROM warnings WHERE guild_id = $1 AND user_id = $2",
+        i64::from(*guild_id),
+        i64::from(*user_id)
+    );
+
+    let rows = select.fetch_all(pool).await?;
+
+    let uuids = rows
+        .iter()
+        .map(|row| Uuid::parse_str(&row.uuid).expect("Failed to parse UUID"))
+        .collect();
+    Ok(uuids)
 }
 
 pub async fn remove_warning_from_db(pool: &PgPool, uuid: &Uuid) -> SqlxThrowable<()> {
     let transaction = pool.begin().await?;
 
-    let delete = sqlx::query("DELETE FROM warnings WHERE uuid = $1")
-        .bind(format!("{}", uuid.simple()))
-        .execute(pool);
+    let delete = sqlx::query!(
+        "DELETE FROM warnings WHERE uuid = $1",
+        format!("{}", uuid.simple())
+    )
+    .execute(pool);
     if let Err(e) = delete.await {
-        tracing::error!("Failed to remove warning from database {e:?}");
+        tracing::error!("Failed to DELETE FROM warnings: {e:?}");
 
         transaction.rollback().await?;
 
@@ -87,18 +91,17 @@ pub async fn add_warning_to_db(
 ) -> SqlxThrowable<()> {
     let transaction = pool.begin().await?;
 
-    let insert = sqlx::query(
+    let insert = sqlx::query!(
         "INSERT INTO warnings (uuid, guild_id, user_id, moderator_id, reason, created_at) VALUES ($1, $2, $3, $4, $5, $6)",
-    )
-    .bind(format!("{}", uuid.simple()))
-    .bind(i64::from(*guild_id))
-    .bind(i64::from(*user_id))
-    .bind(i64::from(*moderator_id))
-    .bind(reason)
-    .bind(Utc::now());
-
-    if let Err(e) = insert.execute(pool).await {
-        tracing::error!("Failed to add warning to database: {e:?}");
+        format!("{}", uuid.simple()),
+        i64::from(*guild_id),
+        i64::from(*user_id),
+        i64::from(*moderator_id),
+        reason,
+        Utc::now().naive_local()
+    ).execute(pool);
+    if let Err(e) = insert.await {
+        tracing::error!("Failed to INSERT INTO warnings: {e:?}");
 
         transaction.rollback().await?;
 
